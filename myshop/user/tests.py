a9af77a3810c5cf0e5f5c17b2a0398c36927f238
taiho <hotai1806@gmail.com>
2019-10-17 18:02:13 +0700

    UIforregister

--- myshop/user/tests.py
+++ myshop/user/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 import math
 # Create your tests here.
+a = int(input("a"))
 
-a = float(5)
-b = float(6)
-c = float(10)
-kq1 = math.sqrt((a*c) - math.pow(b,2))
-kq2 = math.sqrt(math.pow(a,2)+ math.pow(b,2))
-kq = kq1/kq2
-print(kq)
+def ixn(a):
+    if a == 1:
+        return "1"
+    else:
+        return "2"
+    print("avb")
+ixn(a)
--- myshop/user/views.py
+++ myshop/user/views.py
@@ -7,19 +7,46 @@ from django.views.generic import ListView, DetailView, View
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 from .models import Item
 
 
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             user.refresh_from_db()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Your account has been created! You are now able to log in')
+#             return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#     return render(request, 'register.html', {'form': form})
+
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
+        if 'registerbtn' in request.POST:
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                user.refresh_from_db()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Your account has been created! You are now able to log in')
+                return render(request, 'register.html', {'form': form})
+        elif 'loginbtn' in request.POST:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                form = login(request, user)
+                messages.success(request, f' wecome {username} !!')
+                return redirect('user:home')
+            else:
+                messages.info(request, f'account done not exit plz sign in')
+                form = AuthenticationForm()
+            return render(request, 'register.html', {'form': form})
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
